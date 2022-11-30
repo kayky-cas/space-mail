32d2ae2e4ae9e2fb5ec6ffcb011c8e84d77acf8a
Kayky Casagrande <kayky-cas@github.com>
2022-11-30 18:04:59 -0300

    Espaçoporto construtor

--- src/EspacoPorto.java
+++ src/EspacoPorto.java
@@ -11,7 +11,10 @@ public class EspacoPorto {
 	private double coordZ;
 
 	public EspacoPorto(int numero, String nome, double coordX, double coordY, double coordZ) {
-
+		this.numero = numero;
+		this.nome = nome;
+		this.coordX = coordX;
+		this.coordY = coordY;
+		this.coordZ = coordZ;
 	}
-
 }
